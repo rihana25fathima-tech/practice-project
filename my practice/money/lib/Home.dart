24d514b94d728706d74096ed560ec060f735bfa5
Rihana <rihana25fathima@gmail.com>
2026-08-21 09:37:47 +0530

    Update Flutter project

--- my practice/money/lib/Home.dart
+++ my practice/money/lib/Home.dart
@@ -7,166 +7,307 @@ class Homepage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color.fromRGBO(237, 232, 231, 1),
-      body: SafeArea(child: Padding(padding: EdgeInsets.all(20),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
 
-          //topbar
-          Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              CircleAvatar(
-                child: Icon(Icons.person,size: 24,),
-              ),
-              SizedBox(width: 40,),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text("Hello Rihana",style: TextStyle(fontSize: 14),),
-                  Text("Welcome back",style: TextStyle(fontSize: 23,fontWeight: FontWeight.bold),),
-                ],
-              ),
-              SizedBox(width: 90,),
-              Icon(Icons.notifications,size: 20,)
-            ],
-          ),
-SizedBox(height: 22,),
-          // container blue
-
-          Container(
-            decoration: BoxDecoration(
-              color: Colors.blue.shade600,
-              borderRadius: BorderRadius.circular(18),
-            ),
-            width: double.infinity,
-            padding: EdgeInsets.all(10),
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.all(20),
+          child: SingleChildScrollView(
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text("Available Balance",style: TextStyle(fontWeight: FontWeight.bold,color: Colors.white),),
-                SizedBox(height: 8,),
-                Text("8520.00",style: TextStyle(fontWeight: FontWeight.bold,color: Colors.white,fontSize: 20),),
-                SizedBox(height: 40,),
+                //topbar
                 Row(
                   mainAxisAlignment: MainAxisAlignment.start,
                   children: [
-                    Container(
-                      height: 45,
-                      width: 45,
-                      padding: EdgeInsets.all(10),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(15),
-                        color: Colors.white
-                      ),
-                      child: Icon(Icons.add),
+                    CircleAvatar(child: Icon(Icons.person, size: 24)),
+                    SizedBox(width: 40),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text("Hello Rihana", style: TextStyle(fontSize: 14)),
+                        Text(
+                          "Welcome back",
+                          style: TextStyle(
+                            fontSize: 23,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
                     ),
-                    SizedBox(width: 7,),
-                    Text("Details",style: TextStyle(color: Colors.white),),
-                SizedBox(width: 190,),
-                Text("06/20",style: TextStyle(fontWeight: FontWeight.bold,color: Colors.white),),
+                    SizedBox(width: 90),
+                    Icon(Icons.notifications, size: 20),
                   ],
-                )
+                ),
+                SizedBox(height: 22),
+
+                // container blue
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.blue.shade600,
+                    borderRadius: BorderRadius.circular(18),
+                  ),
+                  width: double.infinity,
+                  padding: EdgeInsets.all(10),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Available Balance",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                      SizedBox(height: 8),
+                      Text(
+                        "8520.00",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                          fontSize: 20,
+                        ),
+                      ),
+                      SizedBox(height: 40),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Container(
+                            height: 45,
+                            width: 45,
+                            padding: EdgeInsets.all(10),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(15),
+                              color: Colors.white,
+                            ),
+                            child: Icon(Icons.add),
+                          ),
+                          SizedBox(width: 7),
+                          Text(
+                            "Details",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          SizedBox(width: 190),
+                          Text(
+                            "06/20",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(height: 30),
+
+                //container kayinjtt
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        Container(
+                          height: 45,
+                          width: 45,
+                          padding: EdgeInsets.all(10),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(15),
+                            color: const Color.fromARGB(255, 226, 155, 239),
+                          ),
+                          child: Icon(Icons.telegram),
+                        ),
+                        SizedBox(width: 10),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Transfer",
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 19,
+                              ),
+                            ),
+                            Text(
+                              "0087.00",
+                              style: TextStyle(
+                                fontWeight: FontWeight.normal,
+                                fontSize: 14,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                    SizedBox(width: 50),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Container(
+                          height: 45,
+                          width: 45,
+                          padding: EdgeInsets.all(10),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(15),
+                            color: Colors.greenAccent,
+                          ),
+                          child: Icon(Icons.telegram),
+                        ),
+                        SizedBox(width: 10),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "request",
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 19,
+                              ),
+                            ),
+                            Text(
+                              "0087.00",
+                              style: TextStyle(
+                                fontWeight: FontWeight.normal,
+                                fontSize: 14,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+
+                //send to
+                SizedBox(height: 50),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "Sendto",
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text("view all >", style: TextStyle(color: Colors.blue)),
+                  ],
+                ),
+                SizedBox(height: 17),
+                SizedBox(
+                  height: 90,
+                  child: ListView(
+                    scrollDirection: Axis.horizontal,
+                    children: [
+                      personimage(Icons.person, "Hena"),
+                      personimage(Icons.person, "rihan"),
+                      personimage(Icons.person, "Hena"),
+                      personimage(Icons.person, "rihan"),
+                      personimage(Icons.person, "Hena"),
+                      personimage(Icons.person, "rihan"),
+                      personimage(Icons.person, "Hena"),
+                      personimage(Icons.person, "rihan"),
+                    ],
+                  ),
+                ),
+
+                // transiction
+                SizedBox(height: 30),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "Transiction",
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text("view all >", style: TextStyle(color: Colors.blue)),
+                  ],
+                ),
+
+                //2 containers
+                Container(
+                  padding: EdgeInsets.all(20),
+                  height: 90,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.white,
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Icon(Icons.shopping_bag, size: 20),
+                      SizedBox(width: 30),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [Text("udamy"), Text("subsciption")],
+                      ),
+                      SizedBox(width: 140),
+                      Text("09876"),
+                    ],
+                  ),
+                ),
+
+                SizedBox(height: 30),
+
+                //next
+                Container(
+                  padding: EdgeInsets.all(20),
+                  height: 90,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.white,
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Icon(Icons.shopping_bag, size: 20),
+                      SizedBox(width: 30),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [Text("udamy"), Text("subsciption")],
+                      ),
+                      SizedBox(width: 140),
+                      Text("09876"),
+                    ],
+                  ),
+                ),
               ],
             ),
           ),
-SizedBox(height: 30,),
-          //container kayinjtt
-
-          Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Row(children: [
-              Container(
-                      height: 45,
-                      width: 45,
-                      padding: EdgeInsets.all(10),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(15),
-                        color: const Color.fromARGB(255, 226, 155, 239)
-                      ),
-                      child: Icon(Icons.telegram),
-                    ),
-                    SizedBox(width: 10,),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text("Transfer",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 19,)),
-                         Text("0087.00",style: TextStyle(fontWeight: FontWeight.normal,fontSize: 14,)),
-                      ],
-                    )
-            ],
-          ),
-SizedBox(width: 50,),
-           Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Container(
-                      height: 45,
-                      width: 45,
-                      padding: EdgeInsets.all(10),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(15),
-                        color: Colors.greenAccent,
-                      ),
-                      child: Icon(Icons.telegram),
-                    ),
-                    SizedBox(width: 10,),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text("request",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 19,)),
-                         Text("0087.00",style: TextStyle(fontWeight: FontWeight.normal,fontSize: 14,)),
-                      ],
-                    )
-            ],
-          )
-        ],
+        ),
       ),
 
-//send to
-
-SizedBox(height: 50,),
-Row(
-  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-  children: [ 
-    Text("Sendto"),
-    Text("view all >")
-    ],
-),
-
-Row(
-  mainAxisAlignment: MainAxisAlignment.start,
-  children: [
-    ListView(
-      scrollDirection: Axis.horizontal,
-    children: [
-    personimage(Icon(Icons.person,),Text("hena")),
-     personimage(Icon(Icons.person,),Text("hena")),
-      personimage(Icon(Icons.person,),Text("hena")),
-       personimage(Icon(Icons.person,),Text("hena")),
-        personimage(Icon(Icons.person,),Text("hena")),
-         personimage(Icon(Icons.person,),Text("hena")),
-          personimage(Icon(Icons.person,),Text("hena")),
-         
-  ],
-)
-
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
         ],
-      )],),
-      ),
+        unselectedItemColor: Colors.grey,
+        selectedItemColor: Colors.orange,
       ),
     );
   }
 
-  Widget personimage(IconData icon, String text){
+  Widget personimage(IconData icon, String text) {
     return Container(
       height: 60,
       width: 60,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(17),
+      margin: EdgeInsets.only(right: 15),
+      decoration: BoxDecoration(borderRadius: BorderRadius.circular(17)),
+      child: Column(
+        children: [
+          Icon(icon, size: 31, color: Colors.orange),
+          SizedBox(height: 7),
+          Text(text),
+        ],
       ),
     );
-    
   }
 }
