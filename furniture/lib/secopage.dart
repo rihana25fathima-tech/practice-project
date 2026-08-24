a7c3d98942fc2eed2fa6c6d16133db280f32ab14
Rihana <rihana25fathima@gmail.com>
2026-08-24 20:43:23 +0530

    update project

--- furniture/lib/secopage.dart
+++ furniture/lib/secopage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:furniture/thirdpage.dart';
 
 class Secound_page extends StatefulWidget {
   const Secound_page({super.key});
@@ -8,11 +9,12 @@ class Secound_page extends StatefulWidget {
 }
 
 class _Secound_pageState extends State<Secound_page> {
+  int _selectedIndex = 0;
   final List<Map<String, String>> products = [
     {
       "image": "assets/images/luna.jpg",
       "name": "Luna Sofa",
-      "subname": "Modern 3 Seater Sofa",
+      "subname": "Modern 8 Seater Sofa",
       "price": "₹24,999",
     },
     {
@@ -33,6 +35,47 @@ class _Secound_pageState extends State<Secound_page> {
       "subname": "King Size Bed",
       "price": "₹29,999",
     },
+
+    // extra
+    {
+  "image": "assets/images/board.jpg",
+  "name": "Kitchen Wardrobe",
+  "subname": "Spacious Wardrobe",
+  "price": "₹18,999",
+},
+
+{
+  "image": "assets/images/bookshelf.jpg",
+  "name": "Wooden Bookshelf",
+  "subname": "Modern Storage Shelf",
+  "price": "₹9,499",
+},
+{
+  "image": "assets/images/sidentable.jpg",
+  "name": "Side Table",
+  "subname": "Compact Bedside Table",
+  "price": "₹5,499",
+},
+{
+  "image": "assets/images/tvunit.jpg",
+  "name": "TV Unit",
+  "subname": "Modern Entertainment Unit",
+  "price": "₹14,999",
+},
+{
+  "image": "assets/images/dress ward.jpg",
+  "name": "Wooden Dresser",
+  "subname": "Stylish Bedroom Dresser",
+  "price": "₹17,999",
+},
+{
+  "image": "assets/images/table4.jpg",
+  "name": "Cone Dining Table",
+  "subname": "Modern Round Dining Table",
+  "price": "₹16,999",
+  
+},
+
   ];
 
   @override
@@ -251,9 +294,9 @@ class _Secound_pageState extends State<Secound_page> {
 
                   gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
                     crossAxisCount: 2,
-                    crossAxisSpacing: 15,
-                    mainAxisSpacing: 15,
-                    childAspectRatio: 0.72,
+                    crossAxisSpacing: 10,
+                    mainAxisSpacing: 10,
+                    childAspectRatio: 0.73,
                   ),
                   itemBuilder: (context, index) {
                     final product = products[index];
@@ -271,6 +314,43 @@ class _Secound_pageState extends State<Secound_page> {
           ),
         ),
       ),
+
+      //bottom navigation
+
+    bottomNavigationBar: BottomNavigationBar(
+  currentIndex: _selectedIndex,
+  onTap: (index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  },
+  type: BottomNavigationBarType.fixed,
+  selectedItemColor: Colors.brown,
+  unselectedItemColor: Colors.grey,
+  items: const [
+    BottomNavigationBarItem(
+      icon: Icon(Icons.home_outlined),
+      activeIcon: Icon(Icons.home),
+      label: "Home",
+    ),
+    BottomNavigationBarItem(
+      icon: Icon(Icons.category_outlined),
+      activeIcon: Icon(Icons.category),
+      label: "Categories",
+    ),
+    BottomNavigationBarItem(
+      icon: Icon(Icons.shopping_cart_outlined),
+      activeIcon: Icon(Icons.shopping_cart),
+      label: "Cart",
+    ),
+    BottomNavigationBarItem(
+      icon: Icon(Icons.person_outline),
+      activeIcon: Icon(Icons.person),
+      label: "Profile",
+    ),
+  ],
+),
+
     );
   }
 
@@ -299,13 +379,21 @@ class _Secound_pageState extends State<Secound_page> {
     String subname,
     String price,
   ) {
-    return Container(
+    return GestureDetector(
+      onTap: () {
+        if(name == "Cone Dining Table"){
+          Navigator.push(context, MaterialPageRoute(builder: (context) => Thirdpage(),
+          ),
+          );
+        }
+      },
+child:Container(
       decoration: BoxDecoration(
         color: Colors.white,
         borderRadius: BorderRadius.circular(18),
       ),
       child: Padding(
-        padding: const EdgeInsets.all(10),
+        padding: const EdgeInsets.all(5),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
@@ -335,7 +423,11 @@ class _Secound_pageState extends State<Secound_page> {
 
             Text(
               subname,
-              style: const TextStyle(fontSize: 11, color: Colors.grey),
+              style: const TextStyle(
+                fontSize: 12,
+                color: Colors.grey,
+                fontWeight: FontWeight.bold,
+              ),
             ),
 
             const SizedBox(height: 6),
@@ -351,6 +443,7 @@ class _Secound_pageState extends State<Secound_page> {
           ],
         ),
       ),
+),
     );
   }
 }
---
+++ furniture/lib/thirdpage.dart
@@ -0,0 +1,423 @@
+import 'package:flutter/material.dart';
+
+class Thirdpage extends StatefulWidget {
+  const Thirdpage({super.key});
+
+  @override
+  State<Thirdpage> createState() => _ThirdpageState();
+}
+
+class _ThirdpageState extends State<Thirdpage> {
+  int selectedImage = 0;
+  int selectedColour = 0;
+
+  final List<String> productImages = [
+    "assets/images/table4.jpg",
+    "assets/images/table41.jpg",
+    "assets/images/table42.jpg",
+    "assets/images/table43.jpg",
+  ];
+
+  //colors
+
+  final List<Color> colours = [
+    const Color(0xff75401F),
+    const Color(0xffC7A55D),
+    const Color(0xff80583A),
+    const Color(0xffD5D5D5),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xffF7F1E8),
+      body: SafeArea(
+        child: Column(
+          children: [
+            // ================= TOP BAR =================
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      shape: BoxShape.circle,
+                    ),
+                    child: IconButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      icon: Icon(Icons.arrow_back),
+                    ),
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      shape: BoxShape.circle,
+                    ),
+                    child: IconButton(
+                      onPressed: () {},
+                      icon: Icon(Icons.favorite),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 20),
+            // mail contant
+            Expanded(
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    // ==========MAIN PRODUCT IMAGE =================
+                    Container(
+                      height: 260,
+                      width: double.infinity,
+                      margin: const EdgeInsets.symmetric(horizontal: 15),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(20),
+                        child: Image.asset(
+                          productImages[selectedImage],
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 15),
+
+                    //
+                    SizedBox(
+                      height: 60,
+                      child: ListView.builder(
+                        scrollDirection: Axis.horizontal,
+                        padding: const EdgeInsets.symmetric(horizontal: 20),
+                        itemCount: productImages.length,
+                        itemBuilder: (context, index) {
+                          return GestureDetector(
+                            onTap: () {
+                              setState(() {
+                                selectedImage = index;
+                              });
+                            },
+                            child: Container(
+                              width: 65,
+                              margin: const EdgeInsets.only(right: 10),
+                              padding: const EdgeInsets.all(5),
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.circular(10),
+                                border: Border.all(
+                                  color: selectedImage == index
+                                      ? const Color(0xffD99418)
+                                      : Colors.transparent,
+                                  width: 2,
+                                ),
+                              ),
+                              child: Image.asset(
+                                productImages[index],
+                                fit: BoxFit.contain,
+                              ),
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+
+                    //texts
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 17),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          // CATEGORY
+                          const Text(
+                            "CHAIRS",
+                            style: TextStyle(
+                              fontSize: 12,
+                              color: Color(0xffC18A24),
+                              fontWeight: FontWeight.bold,
+                              letterSpacing: 1,
+                            ),
+                          ),
+                          SizedBox(height: 5),
+
+                          // NAME + PRICE
+                          Row(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              const Expanded(
+                                child: Text(
+                                  "Botero Lounge Chair",
+                                  style: TextStyle(
+                                    fontSize: 25,
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                                ),
+                              ),
+
+                              const SizedBox(width: 10),
+
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.end,
+                                children: [
+                                  const Text(
+                                    "\$890",
+                                    style: TextStyle(
+                                      fontSize: 22,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+
+                                  Text(
+                                    "\$1,100",
+                                    style: TextStyle(
+                                      fontSize: 13,
+                                      color: Colors.grey.shade500,
+                                      decoration: TextDecoration.lineThrough,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+
+                          const SizedBox(height: 5),
+                          Row(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+
+                            children: [
+                              // CATEGORY
+                              const SizedBox(height: 5),
+                              Row(
+                                children: [
+                                  Icon(
+                                    Icons.star,
+                                    color: Color(0xffC9952E),
+                                    size: 17,
+                                  ),
+                                ],
+                              ),
+
+                              const SizedBox(width: 8),
+
+                              const Text(
+                                "4.9 · 248 reviews",
+                                style: TextStyle(
+                                  fontSize: 13,
+                                  color: Colors.grey,
+                                ),
+                              ),
+                            ],
+                          ),
+                          const SizedBox(height: 10),
+
+                          // ================= DESCRIPTION =================
+                          const Text(
+                            "Premium leather, solid wood frame, "
+                            "W72 × D80 × H86cm.",
+                            style: TextStyle(
+                              fontSize: 14,
+                              color: Colors.grey,
+                              height: 1.5,
+                            ),
+                          ),
+                          //colour
+                          const SizedBox(height: 20),
+                          const Text(
+                            "COLOUR",
+                            style: TextStyle(
+                              fontSize: 12,
+                              color: Colors.grey,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          const SizedBox(height: 10),
+
+                          Row(
+                            children: List.generate(colours.length, (index) {
+                              return GestureDetector(
+                                onTap: () {
+                                  setState(() {
+                                    selectedColour = index;
+                                  });
+                                },
+                                child: Container(
+                                  margin: const EdgeInsets.only(right: 12),
+                                  width: 30,
+                                  height: 30,
+                                  padding: const EdgeInsets.all(3),
+                                  decoration: BoxDecoration(
+                                    shape: BoxShape.circle,
+                                    border: Border.all(
+                                      color: selectedColour == index
+                                          ? const Color(0xffD99418)
+                                          : Colors.grey.shade300,
+                                      width: selectedColour == index ? 2 : 1,
+                                    ),
+                                  ),
+                                  child: Container(
+                                    decoration: BoxDecoration(
+                                      color: colours[index],
+                                      shape: BoxShape.circle,
+                                    ),
+                                  ),
+                                ),
+                              );
+                            }),
+                          ),
+
+                          //material
+                          const SizedBox(height: 20),
+                          Text(
+                            "MATERIAL",
+                            style: TextStyle(
+                              fontSize: 12,
+                              color: Colors.grey,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+
+                          SizedBox(height: 10),
+
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              Container(
+                                padding: EdgeInsets.all(5),
+
+                                height: 30,
+                                width: 70,
+                                alignment: Alignment.center,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(15),
+                                  color: const Color.fromARGB(255, 42, 40, 40),
+                                ),
+                                child: Text(
+                                  "wood",
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ),
+                              SizedBox(width: 5),
+                              Container(
+                                padding: EdgeInsets.all(5),
+
+                                height: 30,
+                                width: 70,
+                                alignment: Alignment.center,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(15),
+                                  border: Border.all(color: Colors.black),
+                                ),
+                                child: Text(
+                                  "Leather",
+                                  style: TextStyle(fontWeight: FontWeight.bold),
+                                ),
+                              ),
+                              SizedBox(width: 5),
+                              Container(
+                                padding: EdgeInsets.all(5),
+
+                                height: 30,
+                                width: 70,
+                                alignment: Alignment.center,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(15),
+                                  color: Color(0xffD99418),
+                                  border: Border.all(color: Colors.black),
+                                ),
+                                child: Text(
+                                  "Boucle",
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                          SizedBox(height: 20),
+
+                          Row(
+                            children: [
+                              Expanded(
+                                child: Container(
+                                  height: 50,
+                                  padding: EdgeInsets.all(10),
+                                  decoration: BoxDecoration(
+                                    color: Colors.white,
+                                    borderRadius: BorderRadius.circular(15),
+                                  ),
+                                  child: OutlinedButton(
+                                    onPressed: () {
+                                      ScaffoldMessenger.of(
+                                        context,
+                                      ).showSnackBar(
+                                        SnackBar(
+                                          content: Text(
+                                            "AR View is not available yet",
+                                          ),
+                                          duration: Duration(seconds: 1),
+                                        ),
+                                      );
+                                    },
+                                    child: Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      children: [
+                                        Icon(
+                                          Icons.view_in_ar_outlined,
+                                          size: 18,
+                                          color: Colors.black,
+                                        ),
+                                        Text(
+                                          "View in AR",
+                                          style: TextStyle(color: Colors.black),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                              ),
+
+                              // add to cart
+                              Expanded(
+                                child: Container(
+                                  height: 50,
+                                  padding: EdgeInsets.all(10),
+                                  decoration: BoxDecoration(
+                                    color:Color(0xffD99418),
+                                    borderRadius: BorderRadius.circular(15),
+                                  ),
+                                  child: OutlinedButton(
+                                    onPressed: () {},
+                                    child: Text(
+                                      "Add to Cart",
+                                      style: TextStyle(color: Colors.black),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
