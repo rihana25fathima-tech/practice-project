d3c38a628b738ba9c30c394038576f7e9995c35e
Rihana <rihana25fathima@gmail.com>
2026-09-08 09:55:16 +0530

    practice

---
+++ hive_practice/sharedpre2/android/app/src/main/kotlin/com/example/sharedpre2/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.sharedpre2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ hive_practice/sharedpre2/lib/home.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+class Homepage extends StatefulWidget {
+  const Homepage({super.key});
+
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  TextEditingController nameController = TextEditingController();
+  TextEditingController ageController = TextEditingController();
+List<String>name = [];
+List<String>age=[];
+//save
+Future<void>savedname()async{
+final pref = await SharedPreferences.getInstance();
+pref.setStringList("name", name);
+}
+Future<void>savedage()async{
+final pref = await SharedPreferences.getInstance();
+pref.setStringList("age", age);
+ageController.clear();
+nameController.clear();
+}
+//get
+Future<void>getdname()async{
+final pref = await SharedPreferences.getInstance();
+final named =pref.getStringList("name");
+setState(() {
+ name = named ?? [];
+});
+}
+Future<void>getage()async{
+final pref = await SharedPreferences.getInstance();
+final aged = pref.getStringList("age");
+setState(() {
+  age = aged ?? [];
+});
+}
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(padding: EdgeInsets.all(20),
+      child: Column(
+        children: [
+          TextField(
+            controller: nameController,
+            decoration: InputDecoration(
+              hintText: "name",
+               border: OutlineInputBorder(),
+            ),
+          ),
+          SizedBox(height: 10,),
+           TextField(
+            controller: ageController,
+            decoration: InputDecoration(
+              hintText: "age",
+               border: OutlineInputBorder(),
+            ),
+          ),
+          ElevatedButton(onPressed: () {
+          final sname = nameController.text.trim();
+          final sage = ageController.text.trim();
+            setState(() {
+            name.add(sname);
+            age.add(sage);
+            });
+            savedname();
+            savedage();
+          }, child: Text("save"),),
+          Expanded(child: ListView.builder(itemBuilder: (context, index) {
+            return ListTile(
+              title: Text(name[index]),
+              subtitle: Text(age[index]),
+            );
+          },itemCount: name.length,))
+        ],
+      ),
+      ),
+    );
+  }
+}
