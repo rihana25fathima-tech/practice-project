515e4356049457a59b3be68376673c784be05a37
Rihana <rihana25fathima@gmail.com>
2026-09-10 15:08:14 +0530

    practice

---
+++ badge/todo/lib/home.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+
+class Homepage extends StatefulWidget {
+  const Homepage({super.key});
+
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  TextEditingController nameController =TextEditingController();
+  TextEditingController ageController =TextEditingController();
+  final box = Hive.box('openbox');
+//save
+void saved(){
+  String name = nameController.text.trim();
+  String age = ageController.text.trim();
+  setState(() {
+    box.add({
+     "name" : name,
+     "age" :age,
+    });
+    nameController.clear();
+    ageController.clear();
+  });
+}
+//delete
+Future<void> delete (int index){
+return box.deleteAt(index);
+}
+//get
+String getname(int index){
+return box.getAt(index)["name"];
+}
+
+String getage(int index){
+return box.getAt(index)["age"];
+}
+  //popup
+  void Show(){
+    showDialog(context: context, builder: (context) {
+      return AlertDialog(
+        title: Text("Enter details"),
+        content: Column(
+          children: [
+            TextField(
+              controller: nameController,
+              decoration: InputDecoration(
+                hintText: "enter tour name",
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height: 20,),
+             TextField(
+              controller: ageController,
+              decoration: InputDecoration(
+                hintText: "enter tour age",
+                
+                border: OutlineInputBorder(),
+
+              ),
+            
+            ),
+          SizedBox(height: 10,),
+          ElevatedButton(onPressed: () {
+            saved();
+          }, child: Text("Save"),),
+          ],
+        ),
+         actions: [
+          TextButton(onPressed: () {
+            Navigator.pop(context);
+          }, child: Text("Cancel"))
+         ],
+      );
+    },);
+  }
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+    body: ListView.builder(itemBuilder: (context, index) {
+      return ListTile(
+        title: Text(getname(index)),
+        subtitle: Text(getage(index)),
+        trailing: IconButton(onPressed: () {
+          setState(() {
+            delete(index);
+          });
+        }, icon: Icon(Icons.delete)),
+      );
+    },itemCount: box.length,),
+    floatingActionButton: FloatingActionButton(onPressed:() {
+      Show();
+    }, child: Icon(Icons.add),),
+
+
+    );
+  }
+}
---
+++ badge/todo/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:todo/home.dart';
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
+  await Hive.openBox('openbox');
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+    home: Homepage(),
+    );
+  }
+}
