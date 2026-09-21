6f0bcf476a3560cd303d7abb27ddb85d6902ae57
Rihana <rihana25fathima@gmail.com>
2026-09-21 12:33:05 +0530

    ff

--- badge/todo5/lib/home.dart
+++ badge/todo5/lib/home.dart
@@ -14,74 +14,163 @@ class _HomepagState extends State<Homepag> {
   TextEditingController namecontroller = TextEditingController();
   TextEditingController agecontroller = TextEditingController();
   //save
-  void saved (){
+  void saved() {
     final name = namecontroller.text.trim();
     final age = agecontroller.text.trim();
-    box.add({
-      "name" : name,
-      "age" :age
-    });
+    box.add({"name": name, "age": age});
+    namecontroller.clear();
+    agecontroller.clear();
+    setState(() {});
+  }
+
+  //get
+  String getname(int index) {
+    return box.getAt(index)["name"];
+  }
+
+  String geTage(int index) {
+    return box.getAt(index)["age"];
+  }
+
+  //deelete
+  void delete(int index) {
+    box.deleteAt(index);
+    setState(() {});
+  }
+  //update
+
+  void update(int index) {
+    final name = namecontroller.text.trim();
+    var age = agecontroller.text.trim();
+
+    box.putAt(index, {"name": name, "age": age});
     namecontroller.clear();
     agecontroller.clear();
     setState(() { });
   }
-  //get
-  String getname (int index){
-    return box.getAt(index)["name"];
-  }
-  String geTage(int index){
- return box.getAt(index)["age"];
-  }
-  //alertt
-  void show(){
-    showDialog(context: context, builder: (context) {
-      return AlertDialog(
-        title: Text("Add details"),
-        content: Column(
-          children: [
-            TextField(
-              controller: namecontroller,
-              decoration: InputDecoration(
-                hintText: "enter name",
-                border: OutlineInputBorder(),
+
+  //function update
+
+  void updatee(int index) {
+    namecontroller.text = getname(index);
+    agecontroller.text = geTage(index);
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text("update details"),
+          content: Column(
+            children: [
+              TextField(
+                controller: namecontroller,
+                decoration: InputDecoration(
+                  hintText: "enter name",
+                  border: OutlineInputBorder(),
+                ),
               ),
+              SizedBox(height: 20),
+              TextField(
+                controller: agecontroller,
+                decoration: InputDecoration(
+                  hintText: "enter age",
+                  border: OutlineInputBorder(),
+                ),
+              ),
+            ],
+          ),
+          actions: [
+            ElevatedButton(
+              onPressed: () {
+                update(index);
+                Navigator.pop(context);
+              },
+              child: Text("update"),
             ),
-            SizedBox(height: 20,),
-            TextField(
-               controller: agecontroller,
-              decoration: InputDecoration(
-                hintText: "enter age",
-                border: OutlineInputBorder(),
-              ),
-            )
           ],
-        ),
-       actions: [
-        ElevatedButton(onPressed: () {
-          saved();
-          Navigator.pop(context);
-        }, child: Text("Save"))
-       ],
-      );
-    },);
+        );
+      },
+    );
+  }
+
+  //alertt
+  void show() {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text("Add details"),
+          content: Column(
+            children: [
+              TextField(
+                controller: namecontroller,
+                decoration: InputDecoration(
+                  hintText: "enter name",
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              SizedBox(height: 20),
+              TextField(
+                controller: agecontroller,
+                decoration: InputDecoration(
+                  hintText: "enter age",
+                  border: OutlineInputBorder(),
+                ),
+              ),
+            ],
+          ),
+          actions: [
+            ElevatedButton(
+              onPressed: () {
+                saved();
+                Navigator.pop(context);
+              },
+              child: Text("Save"),
+            ),
+          ],
+        );
+      },
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Details"),
-        centerTitle: true,
+      appBar: AppBar(title: Text("Details"), centerTitle: true),
+      body: ListView.builder(
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(getname(index)),
+            subtitle: Text(geTage(index)),
+
+            trailing: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+
+                IconButton(
+                  onPressed: () {
+                    delete(index);
+                  },
+                  icon: Icon(Icons.delete),
+                ),
+
+                IconButton(
+                  onPressed: () {
+                    updatee(index);
+                  },
+                  icon: Icon(Icons.edit),
+                ),
+              ],
+            ),
+          );
+        },
+        itemCount: box.length,
       ),
-      body:ListView.builder(itemBuilder: (context, index) {
-        return ListTile(
-         title: Text(getname(index)),
-         subtitle: Text(geTage(index)),
-        );
-      },itemCount: box.length,),
-      floatingActionButton: FloatingActionButton(onPressed: () {
-        show();
-      },child: Icon(Icons.add),),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          show();
+        },
+        child: Icon(Icons.add),
+      ),
     );
   }
 }
